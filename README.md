54a3994e76036d4b7a4247b354650fb960977d6d
opensussex <opensussex@gmail.com>
2016-10-07 14:33:58 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # msg
 go messaging backend
+
+the idea is to create a very simple backend that will allow messages to be sent and passed back. 
+
+there will be channels and messages stored in the channel - this will be stored in a sqlite3 db
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Msg struct {
+	gorm.Model
+	Name    string
+	Message string
+	User    string
+	Channel string
+}
+
+type Response struct {
+	Name string
+	Body []string
+}
+
+func message(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Ooops! somethings gone wrong")
+		os.Exit(1)
+	}
+
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, body, "", "\t")
+	if err != nil {
+		fmt.Println("Ooops! somethings gone wrong")
+		os.Exit(1)
+	}
+	fmt.Println("Body: ")
+	fmt.Println(string(prettyJSON.Bytes()))
+	fmt.Println("Params: ")
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	profile := Response{"OK", []string{"Ok", "OK"}}
+
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func main() {
+
+	db := initDb()
+	migrateDB(db)
+	//get the local IP
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println("Ooops! somethings gone wrong")
+		os.Exit(1)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				fmt.Println("Send Requests to: http://" + ipnet.IP.String() + ":8666")
+				//os.Stdout.WriteString(ipnet.IP.String() + "\n")
+			}
+		}
+	}
+	fmt.Println("Ctrl-C exit!")
+	http.HandleFunc("/", welcome)
+	http.HandleFunc("/message", message)
+	err = http.ListenAndServe(":8666", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe: ", err)
+	}
+}
+
+func initDb() *gorm.DB {
+	db, err := gorm.Open("sqlite3", "msg.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
+
+func migrateDB(db *gorm.DB) {
+	db.AutoMigrate(&Msg{})
+}
